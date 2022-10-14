718bc306bf66a1106ecba1580a408be0556e2cd8
erifouD <artmandr@yandex.ru>
2022-10-14 21:17:02 +0300

    VS commit 10/14

---
+++ Source/HW205_rec/UnrealInterface.cpp
@@ -0,0 +1,6 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "UnrealInterface.h"
+
+// Add default functionality here for any IUnrealInterface functions that are not pure virtual.
